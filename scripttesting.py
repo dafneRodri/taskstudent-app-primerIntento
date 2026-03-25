461cae3dec76759df64421718f6d99e053c42f4b
Dafnne <dafnemarelyr@gmail.com>
2026-03-24 22:17:11 -0600

    Fix typo in success message for file check

--- scripttesting.py
+++ scripttesting.py
@@ -6,7 +6,7 @@ print("Iniciando pruebas del proyecto TaskStudentApp")
 
 for archivo in archivos:
     if os.path.exists(archivo):
-        print(archivo + " encontrado correctamente")
+        print(archivo + " encontrado correctamccccccccccente")
     else:
         print("Error: " + archivo + " no encontrado")
 
